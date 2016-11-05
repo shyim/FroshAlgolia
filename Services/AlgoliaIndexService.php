254f3b9f568c2bf03454842c2974c0d109862a12
Soner Sayakci <shyim@outlook.de>
2016-11-05 23:08:03 +0100

    Added instant search for listing

--- Services/AlgoliaIndexService.php
+++ Services/AlgoliaIndexService.php
@@ -5,6 +5,8 @@ use AlgoliaSearch\Client;
 use Doctrine\ORM\AbstractQuery;
 use Shopware\Bundle\StoreFrontBundle\Service\Core\ContextService;
 use Shopware\Bundle\StoreFrontBundle\Service\Core\ProductService;
+use Shopware\Bundle\StoreFrontBundle\Struct\Media;
+use Shopware\Bundle\StoreFrontBundle\Struct\Product;
 use Shopware\Components;
 
 /**
@@ -55,53 +57,54 @@ class AlgoliaIndexService
         $client = new Client($pluginConfig['algolia-application-id'],$pluginConfig['algolia-admin-api-key']);
         $index = $client->initIndex($pluginConfig['index-name']);
 
-        /**
-         * @TODO Create article data here
-         */
-        // Build query
-        $qb = Shopware()->Models()->createQueryBuilder()
-            ->select('article')
-            ->addSelect('detail')
-            ->from('Shopware\Models\Article\Article', 'article')
-            ->where('article.active = :active')
-            ->leftJoin('article.mainDetail', 'detail')
-            ->setParameter('active', true);
+        $articles = Shopware()->Db()->fetchCol('SELECT ordernumber FROM s_articles_details WHERE kind = 1');
 
-        $query = $qb->getQuery();
-        $query->setHydrationMode(AbstractQuery::HYDRATE_OBJECT);
-        $paginator = Shopware()->Container()->get('models')->createPaginator($query);
-        $articles = $paginator->getIterator()->getArrayCopy();
-
-        $data = [];
         $router = Shopware()->Container()->get('router');
+        $data = [];
 
         foreach($articles as $article) {
-
             // Get product object
-            if (!$product = $this->productService->get($article->getMainDetail()->getNumber(), $this->context->getShopContext())):
-                $this->logger->addAlert('Could not generate product struct for article {number} - {articleName} for export. Product not exported.', array('number' => $article->getMainDetail()->getNumber(), 'articleName' => $article->getName()));
+            /** @var Product $product */
+            $product = $this->productService->get($article, $this->context->getShopContext());
+
+            if (!$product) {
+                $this->logger->addAlert('Could not generate product struct for article {number} - {articleName} for export. Product not exported.', array('number' => $article));
                 continue;
-            endif;
+            }
 
             // Get the SEO URL
             $assembleParams = array(
                 'module' => 'frontend',
                 'sViewport' => 'detail',
-                'sArticle' => $article->getId()
+                'sArticle' => $product->getId()
             );
             $link = $router->assemble($assembleParams);
 
+            $media = $product->getMedia();
+            $image = null;
+
+            if (!empty($media)) {
+                /** @var Media $mediaObject */
+                $mediaObject = current($media);
+                $image = $mediaObject->getThumbnail(0)->getSource();
+            }
+
+            $category = $product->getCategories();
+            $category = end($category);
+
             $data[] = array(
-                'objectID' => $article->getMainDetail()->getNumber(),
-                'name' => $article->getName(),
-                'number' => $article->getMainDetail()->getNumber(),
-                'manufacturer_name' => $article->getSupplier()->getName(),
+                'objectID' => $product->getNumber(),
+                'name' => $product->getName(),
+                'number' => $product->getNumber(),
+                'manufacturer_name' => $product->getManufacturer()->getName(),
                 'price' => round($product->getCheapestPrice()->getCalculatedPrice(), 2),
                 'link' => $link,
-                'description' => strip_tags($article->getDescription()),
-                'ean' => $article->getMainDetail()->getEan()
+                'description' => strip_tags($product->getShortDescription()),
+                'ean' => $product->getEan(),
+                'image' => $image,
+                'category' => $category->getName()
             );
-            $this->logger->addInfo('Successfully exported article {number} - {articleName}', array('number' => $article->getMainDetail()->getNumber(), 'articleName' => $article->getName()));
+            $this->logger->addInfo('Successfully exported article {number} - {articleName}', array('number' => $product->getNumber(), 'articleName' => $product->getName()));
         }
 
         $index->addObjects($data);
