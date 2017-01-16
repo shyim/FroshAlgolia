13409bdb1b1f0f7c6eb0c431901d4828c7d50d30
Michael <mfuerst@penandscan.com>
2017-01-16 20:28:25 +0100

    Disabled composer autoload

--- SwAlgolia.php
+++ SwAlgolia.php
@@ -9,7 +9,7 @@ use SwAlgolia\Bootstrap\Schemas;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 // Composer autoload
-require_once __DIR__.'/vendor/autoload.php';
+// require_once __DIR__.'/vendor/autoload.php';
 
 /**
  * Class SwAlgolia.
